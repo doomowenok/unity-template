48f5b7ebcf11707570d0ac2df56b2bc5e7e9c9f0
doomowenok <doomowenok@icloud.com>
2025-02-25 17:57:14 +0300

    Move validation to unit test logic.

--- Game/Assets/Source/Gameplay/Tests/EditMode/ValidationTests.cs
+++ Game/Assets/Source/Gameplay/Tests/EditMode/ValidationTests.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
-using Tools;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Gameplay.Tests.EditMode
 {
@@ -9,8 +15,68 @@ namespace Gameplay.Tests.EditMode
         [Test]
         public void ValidationTestsSimplePasses()
         {
+            bool missingComponentDetected = false;
+            
             // Use the Assert class to test conditions
-            Validator.FindMissingComponents();
+            foreach (Scene scene in GetAllProjectScenes())
+            {
+                foreach (var gameObject in GetAllGameObjects(scene))
+                {
+                    if (HasMissingComponent(gameObject))
+                    {
+                        missingComponentDetected = true;
+                        Logger.LogError("💔", $"GameObject {gameObject.name} from scene {scene.name} has missing components.");     
+                    }
+                }
+            }
+            
+            Assert.That(missingComponentDetected, Is.False);
         }
+        
+        private static ILogger Logger => Debug.unityLogger;
+
+        private static IEnumerable<Scene> GetAllProjectScenes()
+        {
+            string[] scenePaths = AssetDatabase
+                .FindAssets("t:Scene", new[] { "Assets" })
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .ToArray();
+            
+            foreach (string sceneName in scenePaths)
+            {
+                var scene = SceneManager.GetSceneByPath(sceneName);
+                
+                if (scene.isLoaded)
+                {
+                    yield return scene;
+                }
+                else
+                {
+                    Scene openScene = EditorSceneManager.OpenScene(sceneName, OpenSceneMode.Additive);
+                    yield return openScene;
+                    EditorSceneManager.CloseScene(openScene, true);
+                }
+            }
+        }
+
+        private static IEnumerable<GameObject> GetAllGameObjects(Scene scene)
+        {
+            Queue<GameObject> gameObjectsQueue = new Queue<GameObject>(scene.GetRootGameObjects());
+
+            while (gameObjectsQueue.Count > 0)
+            {
+                GameObject gameObject = gameObjectsQueue.Dequeue();
+                
+                yield return gameObject;
+
+                foreach (Transform child in gameObject.transform)
+                {
+                    gameObjectsQueue.Enqueue(child.gameObject);
+                }
+            }
+        }
+        
+        private static bool HasMissingComponent(GameObject gameObject) => 
+            GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(gameObject) > 0;
     }
 }
